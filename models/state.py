5712c9eaf98ec9152a5597eabd14f27c5e8a7b15
Tr3v1n4t0r <813@holbertonschool.com>
2020-03-30 16:31:11 -0700

    forgot to inherit from Base

--- models/state.py
+++ models/state.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
