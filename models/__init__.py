709ae4bdc1f0d638784de397e5ac20692761fc8e
Tr3v1n4t0r <813@holbertonschool.com>
2020-03-30 16:23:13 -0700

    removed extra line

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,6 @@
 """create a unique FileStorage instance for your application"""
 from os import getenv
 
-
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
