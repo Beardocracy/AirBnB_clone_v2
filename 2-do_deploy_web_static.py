33a90867b2a4c371d66d94ad1a696ff83a548bc7
Beardocracy <travisjbearden@gmail.com>
2020-04-07 05:42:29 +0000

    Fixed line too long again'

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,17 +15,17 @@ def do_deploy(archive_path):
         print("Please enter a path to an existing file")
         return False
 
-    archive_full = os.path.basename(archive_path)
-    archive_short = archive_full.split(".")[0]
+    arc_full = os.path.basename(archive_path)
+    arc_short = archive_full.split(".")[0]
 
     put(local_path=archive_path, remote_path="/tmp/")
-    run("mkdir -p /data/web_static/releases/{}".format(archive_short))
-    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(archive_full,
-                                                                  archive_short))
-    run("rm /tmp/{}".format(archive_full))
+    run("mkdir -p /data/web_static/releases/{}".format(arc_short))
+    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(arc_full,
+                                                                  arc_short))
+    run("rm /tmp/{}".format(arc_full))
     run("rm -rf /data/web_static/current")
     run("ln -fs /data/web_static/releases/{}/ \
-        /data/web_static/current".format(archive_short))
+        /data/web_static/current".format(arc_short))
     run("mv /data/web_static/current/web_static/* /data/web_static/current/")
     run("rm -rf /data/web_static/curren/web_static")
 
