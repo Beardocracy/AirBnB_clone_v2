a4b9e6f130174b96d6f4d39a0d7fbb5db21489d7
Beardocracy <travisjbearden@gmail.com>
2020-04-07 04:10:42 +0000

    Fixed pep8:

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,8 +11,13 @@ from datetime import datetime
 def do_pack():
     """ Generate a .tgz archive from the contents of the web_static folder """
     time = datetime.now()
-    name = 'web_static_' + str(time.year) + str(time.month) + str(time.day)
-    name = name + str(time.hour) + str(time.minute) + str(time.second) + '.tgz'
+    name = 'web_static_{}{}{}{}{}{}.tgz'.format(str(time.year),
+                                                str(time.month),
+                                                str(time.day),
+                                                str(time.hour),
+                                                str(time.minute),
+                                                str(time.second)
+                                               )
     local('mkdir -p versions')
     archive = local('tar -cvzf versions/{} web_static'.format(name))
     if archive.failed:
