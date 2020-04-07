9f28dd5c621781f292e8865548e739e91742770a
Beardocracy <travisjbearden@gmail.com>
2020-04-07 03:42:16 +0000

    Added part 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-# Write a Bash script that sets up your web servers for the deployment of web_static
-apt-get -y update
-apt-get -y install nginx
-ufw allow 'Nginx HTTP'
-mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Sub to me at Twitch.tv/tr3v1nat0r" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
-service nginx restart
+# Write a Bash script readies web servers for the deployment of web_static
+
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+sudo mkdir -p /data/web_static/releases/test 
+sudo mkdir /data/web_static/shared
+echo "This is a test file yo" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+sudo service nginx restart
