882da1599e7f66f1ea9ee0709167354146f1fc16
Beardocracy <travisjbearden@gmail.com>
2020-04-24 21:27:42 +0000

    Added values

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_db(error):
+def teardown_db(exception):
         storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_state():
-    states = storage.all(State)
+    states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
 
