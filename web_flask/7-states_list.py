c819d31b2e6e733a09b2339a3e606db73d3c86c4
Beardocracy <travisjbearden@gmail.com>
2020-04-24 21:10:44 +0000

    Fixed issue with state list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,14 +9,16 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_db(error):
-        storage.close()
+def teardown_db(exception):
+    ''' Removes the SQLAlchemy section '''
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+    ''' Show HTML with list of states '''
+    state_list = storage.all(State).values()
+    return render_template('7-states_list.html', states=state_list)
 
 
 if __name__ == '__main__':
